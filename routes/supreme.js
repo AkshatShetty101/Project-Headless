73c4cfe09a190de23709e2c5c175a1cbf17126ff
Akshat Shetty <akshatshetty2908@gmail.com>
2017-06-22 12:23:03 +0530

    Now it is working

--- routes/supreme.js
+++ routes/supreme.js
@@ -1,50 +1,77 @@
 var express = require('express');
 var router = express.Router();
 var Horseman = require('node-horseman');
-// router.use(bodyParser.json());
-// router.use(bodyParser.urlencoded({extended : false}));
-
-
-/* GET users listing. */
+var Jimp = require("jimp");
+var horseman1 =[];
+var fs = require('fs');
+var rn = require('random-number');
+var gen = rn.generator({
+  min:  0
+  , max:  9999
+  , integer: true
+});
 
 router.post('/',function(request,response) {
-var horseman = new Horseman();
-  console.log("HERe!!");
-  //console.log("Iteration:"+s_year.toString());
-  horseman
-  .open('http://www.sci.gov.in/case-status')
-   .type('input[name="partyname"]', 'Akshat')
-  .evaluate(function(){
-    jQuery('#partyname').val("Akshat");
-    jQuery('#ppd').val("D");
-    jQuery('#partyyear').val("2015");
-    jQuery('#party_case_type').val(1);
-    return;
-    //     //console.log("das");
-  })
-   .click('#getPartyData')
-   //.wait(9000)
-   .viewport(3200,1800)
-   .screenshot('img.png')
-   .waitForSelector("#PNdisplay > #cj",{timeout:9000})
-   //.wait(9000)
-  //   .waitFor(function waitForSelector(selector) {
-  //    console.log("IN!!");
-  //      return jQuery(selector).length>0;
-  // }, '#cj',true)
-  .evaluate( function(selector1,selector2,selector3){
-    return {
-      height : jQuery(selector1).val(),
-      height1 : jQuery(selector2).val(),
-      height2 : jQuery(selector3).html()
-    }
-  }, '#ppd','#partyname','#PNdisplay')
-  .then(function(size){
-    console.log("das"+request.body.s_year);
-    response.send(size);
-    // return horseman.close();
-  });
-
+  try{
+    var unique = gen().toString();
+    console.log(unique);
+    var horseman = new Horseman({timeout:20000});
+    var name = request.body.name.toString();
+    var year = request.body.year.toString();
+    console.log(name);
+    console.log("HERe!!");
+    horseman
+    .viewport(3200,1800)
+    .zoom(2)
+    .open('http://www.sci.gov.in/case-status')
+    .type('input[name="partyname"]', 'Akshat')
+    .click('li[data-link="tab3"]')
+    .screenshot('2.png')
+    .evaluate(function(){
+      jQuery('#partyname').val("Akshat");
+      jQuery('#ppd').val("D");
+      jQuery('#partyyear').val("2015");
+      jQuery('#party_case_type').val(1);
+      return;
+      //     //console.log("das");
+    })
+    .click('#getPartyData')
+    .wait(9000)
+    .screenshot('img.png')
+    //.waitForSelector("#PNdisplay br")
+    //  //.wait(9000)
+    .waitFor(function waitForSelector(selector) {
+      if(jQuery('#PNdisplay table').html()!='')
+      {
+        return true;
+      }
+      else
+      if(jQuery('#PNdisplay p').html()!='')
+      {
+        return true;
+      }
+      else {
+        return false;
+      }
+    },'#PNdisplay',true)
+    .screenshot('img1.png')
+    .evaluate( function(selector1,selector2,selector3){
+      return {
+        height : jQuery(selector1).val(),
+        height1 : jQuery(selector2).val(),
+        height2 : jQuery(selector3).html()
+      }
+    }, '#ppd','#partyname','#PNdisplay')
+    .then(function(size){
+      console.log("done!");
+      response.send(size);
+      return horseman.close();
+    });
+  }
+  catch(err)
+  {
+    response.json(err);
+  }
 });
 
 module.exports = router;
