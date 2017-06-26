31db7384236aafe84e3cec7f58739de4aebbdd10
AkshatShetty101 <akshatshetty2908@gmail.com>
2017-06-26 13:13:28 +0530

    Added addCourt , getState and getDistrict functionality

--- routes/map.js
+++ routes/map.js
@@ -20,12 +20,12 @@ router.post('/addDistrict',function(request,response){
     var dcode = request.body.dcode.toString();
     var scode = request.body.scode.toString();
     Map.findOne({'code':scode},function (err,data){
-           if(err)
+        if(err)
             response.json(err);
         else {
             var j ={'name':name,'code':dcode};
             data.district.push(j);
-              data.save(function(err,result){
+            data.save(function(err,result){
                 if(err)
                     response.json(err);
                 else
@@ -37,5 +37,53 @@ router.post('/addDistrict',function(request,response){
         }
     });
 });
+
+router.post('/addCourt',function(request,response){
+    var name = request.body.name.toString();
+    var ccode = request.body.ccode.toString();
+    var dcode = request.body.dcode.toString();
+    var scode = request.body.scode.toString();
+    Map.findOne( { 'code' : scode },{ 'district': { '$elemMatch': { 'code': dcode } } },function (err,data){
+        if(err)
+            response.json(err);
+        else {
+             console.log(data);
+                    var j ={'name':name,'code':ccode};
+                    data.district[0].court.push(j);
+                    data.save(function(err,result){
+                        if(err)
+                            response.json(err);
+                        else
+                        {
+                            console.log(result);
+                            response.json(data);
+                        }
+                    });
+
+        }
+    });
+});
+
+router.get('/getState',function(request,response){
+    Map.find({},{"name":"1","code":"1"},function (err,data) {
+        if (err)
+            response.json(err);
+        else {
+            response.json(data);
+        }
+    });
+});
+
+router.post('/getDistrict',function(request,response){
+    var scode = request.body.scode.toString();
+    Map.find({ 'code' : scode },{'district.name':"1",'district.code':"1"},function (err,data){
+        if (err)
+            response.json(err);
+        else {
+            response.json(data[0].district);
+        }
+    });
+});
+
 
 module.exports =router;
