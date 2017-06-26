c50e6b0f27e9a28f3fae9f2cb674f58315970e5d
Adhrit Shetty <adhritshetty2908@gmail.com>
2017-06-26 09:56:38 +0530

    Removed Stack

--- Angular/src/app/Ecourt/input.component.ts
+++ Angular/src/app/Ecourt/input.component.ts
@@ -14,9 +14,9 @@ import {Observable} from "rxjs/Observable";
 })
 export class InputComponent implements OnInit {
 
-  stackname: any[] = new Array(1);
-  stackupper: any[] = new Array(1);
-  stacklower: any[] = new Array(1);
+  stackname: any;
+  stackupper: any;
+  stacklower: any;
   captcha: any[] = new Array(1);
   captcha_response: any[] = new Array(1);
   invalid: any[] = new Array(0);
@@ -26,7 +26,6 @@ export class InputComponent implements OnInit {
   myCaptcha: FormGroup;
   myCaptchaInvalid: FormGroup;
 
-  k:number = 0;
   flag:any = 0;
   recaptcha: any;
   count : any = 0;
@@ -55,9 +54,6 @@ export class InputComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.stackinit(this.stackname);
-    this.stackinit(this.stackupper);
-    this.stackinit(this.stacklower);
     this.logic.initRecords();
   }
 
@@ -70,9 +66,10 @@ export class InputComponent implements OnInit {
   }
 
   push(data:any){
-    this.stackname[this.k] = data.name;
-    this.stackupper[this.k] = data.year_upper;
-    this.stacklower[this.k] = data.year_lower;
+    this.stackname = data.name;
+    this.stackupper = data.year_upper;
+    this.stacklower = data.year_lower;
+    this.onSearch();
   }
 
   printstack(stack: any[]){
@@ -87,22 +84,21 @@ export class InputComponent implements OnInit {
     console.log('Submitted');
     let i: any, limit: number;
     let request: any[] = new Array(1);
-    //for(i=0; i<this.stackname.length; i++){
-      console.log(this.stackname[0]);
-      console.log(this.stackupper[0]);
-      console.log(this.stacklower[0]);
-      limit = this.stackupper[0]-this.stacklower[0];
-      for(i=0; i <= limit; i++){
-          request[i] = {
-          name : this.stackname[0],
-          year : (parseInt(this.stacklower[0]) + parseInt(i)).toString(),
-          val1 : "1~Regional Language~0~~~marathi",
-          val2 : "37",
-          val3 : "119@1,2"
-        };
-      }
-      console.log(request);
-      this.sendMultiple(request);
+    console.log(this.stackname);
+    console.log(this.stackupper);
+    console.log(this.stacklower);
+    limit = this.stackupper-this.stacklower;
+    for(i=0; i <= limit; i++){
+      request[i] = {
+        name : this.stackname,
+        year : (parseInt(this.stacklower) + parseInt(i)).toString(),
+        val1 : "1~Regional Language~0~~~marathi",
+        val2 : "37",
+        val3 : "119@1,2"
+      };
+    }
+    console.log(request);
+    this.sendMultiple(request);
   }
 
   //"val1":"1~Regional Language~0~~~marathi","val2":"37","val3":"119@1,2","name":"Akshat","year":"2016"}
