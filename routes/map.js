f9b7bc701b2cd60551af1b0b1cb3d249c6374cf4
AkshatShetty101 <akshatshetty2908@gmail.com>
2017-06-26 18:36:47 +0530

    Added functionality to read stuff from text and store in db

--- routes/map.js
+++ routes/map.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var path = require('path');
 var router = express.Router();
 var Map = require('../models/map');
+var lineReader = require('readline');
 
 router.get('/addStatePage',function(request,response){
     var link="../public/html/addState.html";
@@ -72,22 +73,45 @@ router.post('/addCourt',function(request,response){
         if(err)
             response.json(err);
         else {
-             console.log(data);
-                    var j ={'name':name,'code':ccode};
-                    data.district[0].court.push(j);
-                    data.save(function(err,result){
-                        if(err)
-                            response.json(err);
-                        else
-                        {
-                            console.log(result);
-                            response.json(data);
-                        }
-                    });
+            console.log(data);
+            var j ={'name':name,'code':ccode};
+            data.district[0].court.push(j);
+            data.save(function(err,result){
+                if(err)
+                    response.json(err);
+                else
+                {
+                    console.log(result);
+                    response.json(data);
+                }
+            });
 
         }
     });
 });
+
+router.post('/deleteCourts',function(request,response){
+    var dcode = request.body.dcode.toString();
+    var scode = request.body.scode.toString();
+    Map.findOne({'code' : scode},{'district':{'$elemMatch':{'code': dcode}}},function (err,data){
+        if(err)
+            response.json(err);
+        else {
+            console.log(data);
+            data.district[0].court=[];
+            data.save(function(err,result){
+                if(err)
+                    response.json(err);
+                else
+                {
+                    console.log(result);
+                    response.json(data);
+                }
+            });
+        }
+    });
+});
+
 
 router.get('/getState',function(request,response){
     Map.find({},{"name":"1","code":"1"},function (err,data) {
@@ -122,6 +146,118 @@ router.post('/getCourt',function(request,response){
     });
 });
 
-router.post('/getCourt',function(request,response) {
+router.get('/adc',function(request,response) {
+    var splitString4=[];
+    var a =lineReader.createInterface({
+        input: fs.createReadStream('./a.txt')
+    });
+    var ct=0;
+    var scode;
+    var dcode;
+    a.on('line', function (line) {
+        if(ct==0)
+        {
+            scode = line;
+        }
+        else
+        if (ct==1)
+        {
+            dcode = line;
+        }
+        else
+        {
+            Map.findOne({'code' : scode},{'district':{'$elemMatch':{'code': dcode}}},function (err,data){
+                if(err)
+                    response.json(err);
+                else {
+                    console.log('Line from file:', line);
+                    splitString4 = line.split(/\s*>\s*/);
+                    console.log(splitString4[1]);
+                    console.log(scode);
+                    console.log(dcode);
+                    console.log(data);
+                    var j ={'name':splitString4[1],'code':splitString4[0]};
+                    data.district[0].court.push(j);
+                    data.save(function(err,result){
+                        if(err)
+                            response.json(err);
+                        else
+                        {
+                            console.log(result);
+                        }
+                    });
+                }
+            });
+        }
+        ct++;
+    });
+    response.json('done');
+});
+
+router.get('/g',function(request,response) {
+    var splitString4=[];
+    var a =lineReader.createInterface({
+        input: fs.createReadStream('./a.txt')
+    });
+    var ct=0;
+    var scode;
+    a.on('line', function (line) {
+        if(ct==0)
+        {
+            scode = line;
+        }
+        else
+        {
+                    console.log('Line from file:', line);
+                    // splitString4 = line.split(/([>])(?:(?=(\\?))\2.)*?\1/);
+                    splitString4 = line.split(/>^\s*>/);
+                    console.log(splitString4);
+
+        }
+        ct++;
+    });
+    response.json('done');
+});
+
+router.get('/add',function(request,response) {
+    var splitString4=[];
+    var a =lineReader.createInterface({
+        input: fs.createReadStream('./a.txt')
+    });
+    var ct=0;
+    var scode;
+    a.on('line', function (line) {
+        if(ct==0)
+        {
+            scode = line;
+        }
+        else
+        {
+            Map.findOne({'code' : scode},function (err,data){
+                if(err)
+                    response.json(err);
+                else {
+                    console.log('Line from file:', line);
+                    splitString4 = line.split(/\s*>\s*/);
+                    console.log(splitString4[1]);
+                    console.log(scode);
+                    console.log(data);
+                    var j ={'name':splitString4[1],'code':splitString4[0]};
+                    data.district.push(j);
+                    data.save(function(err,result){
+                        if(err)
+                            response.json(err);
+                        else
+                        {
+                            console.log(result);
+                        }
+                    });
+
+                }
+            });
+        }
+        ct++;
+    });
+    response.json('done');
 });
 module.exports =router;
