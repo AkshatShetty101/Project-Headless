27faeadc6d268ff68afa87f7f2a5559885a4a47d
Adhrit Shetty <adhritshetty2908@gmail.com>
2017-07-04 10:42:55 +0530

    Login Page

--- Phase-2/Angular/src/app/app.module.ts
+++ Phase-2/Angular/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {LogicService} from "./Shared/logic.service";
 import {DeactivateGuard} from "./Shared/deactivate.guard";
 import {ActivateGuard} from "./Shared/activate.guard";
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
 
 
@@ -21,7 +22,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- Phase-2/Angular/src/app/app.routing.ts
+++ Phase-2/Angular/src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home.component";
+import {LoginComponent} from "./login/login.component";
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'eCourt', loadChildren: 'app/Ecourt/ecourt.module#EcourtModule'},
+  { path: 'login', component: LoginComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', redirectTo: 'home', pathMatch: 'full'},
 ];
