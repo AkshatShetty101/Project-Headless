df20fa0c5ba69689e1be63e2ba1b269d48fc4f30
Adhrit Shetty <adhritshetty2908@gmail.com>
2017-07-06 11:41:29 +0530

    View User Page

--- Phase-2/Angular/src/app/admin/admin.module.ts
+++ Phase-2/Angular/src/app/admin/admin.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import {adminRouting} from "./admin.routing";
 import { AdminlandingComponent } from './adminlanding.component';
 import { ViewuserComponent } from './viewuser.component';
+import { AdduserComponent } from './adduser.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     adminRouting
   ],
-  declarations: [AdminlandingComponent, ViewuserComponent]
+  declarations: [AdminlandingComponent, ViewuserComponent, AdduserComponent]
 })
 export class AdminModule { }
