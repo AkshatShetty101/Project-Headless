2cfc8febb83d7ff95fdaf0741dcbdbad4bc95c35
AkshatShetty101 <akshatshetty2908@gmail.com>
2017-07-10 12:38:59 +0530

    random

--- Phase-1/routes/worker.js
+++ Phase-1/routes/worker.js
@@ -1,37 +1,37 @@
-var Worker = require('webworker-threads').Worker;
-// var w = new Worker('worker.js'); // Standard API
-
-// You may also pass in a function:
-var worker = new Worker(function(){
-  postMessage("I'm working before postMessage('ali').");
-  this.onmessage = function(event) {
-    postMessage('Hi ' + event.data);
-    self.close();
-  };
-});
-worker.onmessage = function(event) {
-  console.log("Worker said : " + event.data);
-};
-worker.postMessage('ali');
-function fibo (n) {
-  return n > 1 ? fibo(n - 1) + fibo(n - 2) : 1;
-}
-
-function cb (err, data) {
-  if(err)
-  throw err;
-  process.stdout.write(" ["+ this.id+ "]"+ data);
-  //this.eval('fibo(35)', cb);
-}
-
-var Threads= require('webworker-threads');
-
-Threads.create().eval(fibo).eval('fibo(35)', cb);
-Threads.create().eval(fibo).eval('fibo(35)', cb);
-Threads.create().eval(fibo).eval('fibo(35)', cb);
-Threads.create().eval(fibo).eval('fibo(35)', cb);
-Threads.create().eval(fibo).eval('fibo(35)', cb);
-
-// (function spinForever () {
-//   setImmediate(spinForever);
-// })();
+// var Worker = require('webworker-threads').Worker;
+// // var w = new Worker('worker.js'); // Standard API
+//
+// // You may also pass in a function:
+// var worker = new Worker(function(){
+//   postMessage("I'm working before postMessage('ali').");
+//   this.onmessage = function(event) {
+//     postMessage('Hi ' + event.data);
+//     self.close();
+//   };
+// });
+// worker.onmessage = function(event) {
+//   console.log("Worker said : " + event.data);
+// };
+// worker.postMessage('ali');
+// function fibo (n) {
+//   return n > 1 ? fibo(n - 1) + fibo(n - 2) : 1;
+// }
+//
+// function cb (err, data) {
+//   if(err)
+//   throw err;
+//   process.stdout.write(" ["+ this.id+ "]"+ data);
+//   //this.eval('fibo(35)', cb);
+// }
+//
+// var Threads= require('webworker-threads');
+//
+// Threads.create().eval(fibo).eval('fibo(35)', cb);
+// Threads.create().eval(fibo).eval('fibo(35)', cb);
+// Threads.create().eval(fibo).eval('fibo(35)', cb);
+// Threads.create().eval(fibo).eval('fibo(35)', cb);
+// Threads.create().eval(fibo).eval('fibo(35)', cb);
+//
+// // (function spinForever () {
+// //   setImmediate(spinForever);
+// // })();
